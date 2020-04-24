d2d1072743dc8218e3cf6d3447df9d13a60e0546
Vanessa Mercado <vanessa.mercado24@gmail.com>
2020-04-23 18:11:10 -0700

    base commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+const server = express();
+
+server.get("/", (res, req) => {
+  const packageJson = require("./package.json");
+  res.json(packageJson);
+});
+
+server.listen(3000, () => {
+  console.log("server is on");
+});
